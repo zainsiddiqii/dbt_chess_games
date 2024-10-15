4353affddbbb0d85460449d2a67e970d9c3810d7
Zain Ibrahim Siddiqi <zain_siddiqi@outlook.com>
2024-10-15 16:06:04 +0000

    Added schema for fct_game, fixed code for ref to dim_opening

--- models/marts/fct_game.sql
+++ models/marts/fct_game.sql
@@ -1,19 +1,5 @@
 with
 
-columns_to_join_on as (
-  select
-    game_id,
-    opening_name,
-    opponent_id,
-    my_result,
-    result_method,
-    colour,
-    time_class,
-    time_control
-
-  from {{ ref("int_game_information") }}
-),
-
 joining_dims as (
   select
     cols.game_id,
@@ -21,9 +7,24 @@ joining_dims as (
     dim_opening.opening_sid,
     dim_opponent.opponent_sid,
     dim_result.result_sid,
-    dim_time_control.time_control_sid
+    dim_time_control.time_control_sid,
+    cols.my_rating,
+    cols.opponent_rating,
+    cols.my_accuracy,
+    cols.opponent_accuracy,
+    cols.move_number_reached,
+    cols.total_moves,
+    datetime_diff(
+      cols.end_timestamp_utc,
+      cols.start_timestamp_utc,
+      second
+    )                  as game_length,
+    lead(cols.my_rating, 1) over (
+      partition by cols.source
+      order by cols.start_timestamp_utc
+    ) - cols.my_rating as rating_change
 
-  from columns_to_join_on as cols
+  from {{ ref("int_game_information") }} as cols
 
   left join {{ ref("dim_game") }} as dim_game
     on cols.game_id = dim_game.game_id
@@ -33,6 +34,10 @@ joining_dims as (
     = trim(split(cols.opening_name, ':')[safe_offset(0)])
     and coalesce(dim_opening.opening_variation, '')
     = coalesce(trim(split(cols.opening_name, ':')[safe_offset(1)]), '')
+    and dim_opening.white_first_move
+    = cols.moves[safe_offset(0)]
+    and dim_opening.black_first_move
+    = cols.moves[safe_offset(1)]
 
   left join {{ ref("dim_opponent") }} as dim_opponent
     on cols.opponent_id = dim_opponent.opponent_id
@@ -43,28 +48,6 @@ joining_dims as (
 
   left join {{ ref("dim_time_control") }} as dim_time_control
     on cols.time_control = dim_time_control.time_control
-),
-
-fact_columns as (
-  select
-    game_id,
-    my_rating,
-    opponent_rating,
-    my_accuracy,
-    opponent_accuracy,
-    move_number_reached,
-    total_moves,
-    datetime_diff(
-      end_timestamp_utc,
-      start_timestamp_utc,
-      second
-    )             as game_length,
-    lead(my_rating, 1) over (
-      partition by source
-      order by start_timestamp_utc
-    ) - my_rating as rating_change
-
-  from {{ ref("int_game_information") }}
 ),
 
 fct_game as (
@@ -83,10 +66,7 @@ fct_game as (
     game_length,
     rating_change
 
-  from fact_columns
-
-  inner join joining_dims
-    on fact_columns.game_id = joining_dims.game_id
+  from joining_dims
 )
 
 select * from fct_game
