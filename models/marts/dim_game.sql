f70e4c35c675f56facdd1ce816aaf36bf79949f2
Zain Ibrahim Siddiqi <zain_siddiqi@outlook.com>
2024-10-15 12:22:24 +0000

    Added generate_id cte for column ordering

--- models/marts/dim_game.sql
+++ models/marts/dim_game.sql
@@ -6,6 +6,7 @@ unique_games as (
     game_url,
     source,
     is_rated,
+    colour,
     date(start_timestamp_utc)   as start_date_utc,
     date(start_datetime_actual) as start_date_actual,
     time(start_timestamp_utc)   as start_time_utc,
@@ -13,18 +14,37 @@ unique_games as (
     date(end_timestamp_utc)     as end_date_utc,
     date(end_datetime_actual)   as end_date_actual,
     time(end_timestamp_utc)     as end_time_utc,
-    time(end_datetime_actual)   as end_time_actual,
-    colour
+    time(end_datetime_actual)   as end_time_actual
 
   from {{ ref("int_game_information") }}
 ),
 
-dim_game as (
+generate_id as (
   select
     *,
     generate_uuid() as game_sid
 
   from unique_games
+),
+
+dim_game as (
+  select
+    game_sid,
+    game_id,
+    game_url,
+    source,
+    is_rated,
+    start_date_utc,
+    start_date_actual,
+    start_time_utc,
+    start_time_actual,
+    end_date_utc,
+    end_date_actual,
+    end_time_utc,
+    end_time_actual,
+    colour
+
+  from generate_id
 )
 
 select * from dim_game
